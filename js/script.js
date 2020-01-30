c624e5cb04c9c3e68f796c6830f7c832a4ad97c6
Davide Lugli <luglidavide.web@gmail.com>
2020-01-30 16:08:40 +0100

    fine

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready( function() {
+$('.square').click(
+  function () {
+  var thisSquare = $(this);
+
+    $.ajax({
+      url: "https://flynn.boolean.careers/exercises/api/random/int",
+      method: "GET",
+      success: function(data, stato) {
+        $("#risultati").html(data);
+        console.log(data.response);
+        if (data.response <= 5) {
+          thisSquare.addClass('yellow');
+          thisSquare.text(data.response)
+
+        } else if (data.response > 5) {
+          thisSquare.addClass('green');
+          thisSquare.text(data.response)
+        }
+      },
+      error: function(richiesta, stato, errori) {
+        alert("E ' avvenuto un errore. " + errore);
+      }
+    });
+
+  }
+);
+});
