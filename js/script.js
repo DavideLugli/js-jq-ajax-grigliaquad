c2a5777045041409111ae81d49c231650447978b
Davide Lugli <luglidavide.web@gmail.com>
2020-01-31 10:41:21 +0100

    validation js (correzione con Chiara)

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
-$(document).ready( function() {
-$('.square').click(
-  function () {
-  var thisSquare = $(this);
+$(document).ready(function() {
+  $('.square').click(
+    function() {
+      var thisSquare = $(this);
+      if (!thisSquare.hasClass('yellow') && !thisSquare.hasClass('green')) {
+        $.ajax({
+          url: "https://flynn.boolean.careers/exercises/api/random/int",
+          method: "GET",
+          success: function(data, stato) {
+            $("#risultati").html(data);
+            console.log(data.response);
+            if (data.response <= 5) {
+              thisSquare.addClass('yellow');
+              thisSquare.text(data.response)
 
-    $.ajax({
-      url: "https://flynn.boolean.careers/exercises/api/random/int",
-      method: "GET",
-      success: function(data, stato) {
-        $("#risultati").html(data);
-        console.log(data.response);
-        if (data.response <= 5) {
-          thisSquare.addClass('yellow');
-          thisSquare.text(data.response)
-
-        } else if (data.response > 5) {
-          thisSquare.addClass('green');
-          thisSquare.text(data.response)
-        }
-      },
-      error: function(richiesta, stato, errori) {
-        alert("E ' avvenuto un errore. " + errore);
+            } else if (data.response > 5) {
+              thisSquare.addClass('green');
+              thisSquare.text(data.response)
+            }
+          },
+          error: function(richiesta, stato, errori) {
+            alert("E ' avvenuto un errore. " + errore);
+          }
+        });
       }
-    });
-
-  }
-);
+    }
+  );
 });
